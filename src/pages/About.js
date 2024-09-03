14786ae21a012c59306e1160da2803039c873fb3
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-09-03 18:29:05 +0300

    Complete about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,6 +3,7 @@ import Navigation from "../components/Navigation";
 import "./About.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUsers, faFile } from "@fortawesome/free-solid-svg-icons";
+import Footer from "../components/Footer";
 
 function About() {
   return (
@@ -53,16 +54,63 @@ function About() {
         </div>
       </div>
       <div className="client-first">
-        <div className="client-first-content">
-          <h6>Use Client-first</h6>
-          <h2>Top agencies and freelancers around the world use client-first</h2>
-          <p>
-            This is to prioritize the unique needs and goals of my clients, ensuring tailored solutions and 
-            maximum satisfaction. 
-          </p>
+        <div className="client-first-container">
+          <div className="client-first-content">
+            <h6>
+              Use Client-first
+            </h6>
+            <h2>Top agencies and freelancers around the world use client-first</h2>
+            <p>
+              This is to prioritize the unique needs and goals of my clients, ensuring tailored solutions and 
+              maximum satisfaction. 
+            </p>
+          </div>
+          <div className="client-first-image"></div>
         </div>
-        <div className="client-first-image"></div>
-      </div>        
+      </div>
+      <div className="revision-rounds-and-maintenance">
+        <div className='revision-rounds-and-maintenance-container'>
+          <div className='maintenance-image'>
+            <img src="https://images.pexels.com/photos/5685937/pexels-photo-5685937.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=''/>
+          </div>
+          <div className='maintenance-content'>
+            <h6>Revision Rounds</h6>
+            <h2>
+              Get two weeks free Revisions and Maintenance.
+            </h2>
+            <p>
+              For the first two weeks after deployment,you get free revisions and maintenance from me!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="support">
+        <div className="support-container">
+          <div className='support-content'>
+            <h2>
+              24/7 Support
+            </h2>
+            <p>By working with me, you will be getting 24/7 priority support</p> 
+          </div>
+          <div className='support-image'>
+            <img src='https://images.pexels.com/photos/7681839/pexels-photo-7681839.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' />
+          </div>
+        </div>
+      </div>
+      <div  className="quick-delivery">
+        <div className="quick-delivery-container">
+          <div className='delivery-image'>
+            <img src="https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=''/>
+          </div>
+          <div className='delivery-content'>
+            <h2>Quick Delivery</h2>
+            <p>
+              You are guaranteed timely delivery of the service as per the standerdised web pages.
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer/>
     </div>
   )
 }
