a72f7374f1418a790c2505edd4969cf55cc016ac
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-09-03 17:18:56 +0300

    Navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,55 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from "../logo.PNG";
+import { Link } from 'react-router-dom';
+
+
+function CollapsibleExample() {
+  return (
+    <Navbar collapseOnSelect expand="lg" className="bg-body-white">
+      <Container>
+        <Navbar.Brand href="#home"><img  src={logo} alt="logo" style={{ width: 70,  height: 70 }}/></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+         <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/" className="page text-decoration-none text-white">
+              Home
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/about"
+              className="page text-decoration-none text-white"
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/services"
+              className="page text-decoration-none text-white"
+            >
+              Services
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/project"
+              className="page text-decoration-none text-white"
+            >
+              Projects
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/contact"
+              className="page text-decoration-none text-white"
+            >
+              Contacts
+            </Nav.Link>
+          </Nav>
+         </Navbar.Collapse>
+         
+      </Container>
+    </Navbar>
+  );
+}
+
+export default CollapsibleExample;
