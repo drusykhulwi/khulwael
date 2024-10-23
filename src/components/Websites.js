02616a5e97e56fe12bba29f589593065fafd0f4f
Drusilla Khulwael <102147801+drusykhulwi@users.noreply.github.com>
2024-10-23 14:17:04 +0300

    project 2

--- src/components/Websites.js
+++ src/components/Websites.js
@@ -18,13 +18,11 @@ function Websites() {
         </div>
         <div className="website-project">
           <img src={Weather} alt="weather app"/>
-          <h3>Weather App</h3>
+          <h3>Shoees shop</h3>
           <p>
-            This is a weather app developed using React.js.
-            It is used to assist the user to get real-time weather forecast of
-            different cities.
+            This is a shoe shop website developed using php
           </p>
-          <a href="https://drusilla-weather-app.vercel.app/" rel="noreferrer" target="_blank">View Project</a>
+          <a href="druel.great-site.net" rel="noreferrer" target="_blank">View Project</a>
         </div>
         <div className="website-project">
           <img src={Weather} alt="weather app"/>
